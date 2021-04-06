cc7988a5b8452bbf3f3a093967887e6ee6440f69
최서정 <cchloe2311@daum.net>
2021-04-06 18:26:54 +0900

    used MainScope instead of GlobalScope

--- app/src/main/java/com/example/flowpractice/MainActivity.kt
+++ app/src/main/java/com/example/flowpractice/MainActivity.kt
@@ -3,12 +3,12 @@ package com.example.flowpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val scope = MainScope() // Creates the main CoroutineScope for UI components. The resulting scope has SupervisorJob and Dispatchers.Main context elements.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +17,16 @@ class MainActivity : AppCompatActivity() {
         val flowPracticeRemoteDataSource = FlowPracticeRemoteDataSource()
         val flowPracticeRepository = FlowPracticeRepository(flowPracticeRemoteDataSource)
 
-        // TODO GlobalScope를 쓰는게 맞나?
-        GlobalScope.launch(Dispatchers.Main) { // if no Dispatchers.Main -> CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
+        scope.launch {
             flowPracticeRepository.count.collect { // collect는 반드시 코루틴이나 다른 suspend function에서 호출되어야 함
                 tv_count.text = it
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        scope.cancel()
     }
 }
